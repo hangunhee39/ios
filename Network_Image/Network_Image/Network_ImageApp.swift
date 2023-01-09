19f97709669c08e5c33961ded445bf1060c3136c
hangunhee39 <ghee0831@naver.com>
2023-01-09 23:31:03 +0900

    2 -s
    api 로 사진 받는 어플

---
+++ Network_Image/Network_Image/Network_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_ImageApp.swift
+//  Network_Image
+//
+//  Created by hangeonhui on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct Network_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
